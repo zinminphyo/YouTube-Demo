2ed5f665c0e52c27bc627055d7e96ef288aa990b
Zin Min Phyoe <zinminphyo075@gmail.com>
2018-11-12 18:02:39 +0630

    implement first step

---
+++ YouTube-Demo/YouTube-Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YouTube-Demo
+//
+//  Created by Zin Min Phyoe on 11/12/18.
+//  Copyright © 2018 Zin Min Phyoe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
